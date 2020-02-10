9d1d41fde75e6f59a917094e65c576d13f67cdde
Harrison Clark <harrison.clark99@icloud.com>
2020-02-10 17:56:18 -0500

    Updated url paths between apps

--- study/models.py
+++ study/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class StudentRatings(models.Model):
+    rating = models.IntegerField()
+    comment = models.CharField(max_length=200)
+    # student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    # rater = models.ForeignKey(Rating)
+
