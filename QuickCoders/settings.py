9a9bfb9dcab5c0abbc14a69530fb41a9eb2d0997
Kelly Chen <kellyjchen@gmail.com>
2020-02-15 04:11:15 -0500

    Add bootstrap and make temporary login page.

--- QuickCoders/settings.py
+++ QuickCoders/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -56,7 +59,8 @@ ROOT_URLCONF = 'QuickCoders.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'login/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                os.path.join(BASE_DIR,'login/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,5 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, "login/static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'login/static', 'static_root')
+
 
 LOGIN_REDIRECT_URL = '/'
