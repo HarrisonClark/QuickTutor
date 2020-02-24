eff73d5482056d517247d6adbd3432207ed6166a
hpathipati <hp4hz@virginia.edu>
2020-02-24 18:15:40 -0500

    user profile

--- study/models.py
+++ study/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class StudentRatings(models.Model):
     rating = models.IntegerField()
@@ -16,7 +18,15 @@ class Student(models.Model):
     student_year = models.CharField(max_length=50)
     email = models.CharField(max_length=100)
     picture = models.URLField()
-    bio = models.CharField(max_length=200)
+    bio = models.CharField(max_length=200, blank = True)
     rating = models.ForeignKey(StudentRatings, on_delete=models.CASCADE)
 
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        new_student = Student(user=instance)
+        new_student.save()
 
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.student.save()
