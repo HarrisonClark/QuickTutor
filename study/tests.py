1fdcbdc3e67ac363d3fa2197c1c5d8a92e56ad5a
hpathipati <hp4hz@virginia.edu>
2020-02-29 16:24:49 -0500

    added a test

--- study/tests.py
+++ study/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import StudentRatings, Student
 
-# Create your tests here.
+class TestStudentRatings(TestCase):
+    def setUp(self):
+        StudentRatings.objects.create(rating=5, comment="Excellent!")
+    def test_specific_rating(self):
+        stu_comment = StudentRatings.objects.get(comment="Excellence")
+        self.assertEqual(stu_comment.review_sentence(), 'This student has a rating of 5, and a student said \"excellent\" about this tutor')
