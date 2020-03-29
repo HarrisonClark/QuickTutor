e5419014372ac3ef333962482c1be8f668897058
Harrison Clark <harrison.clark99@icloud.com>
2020-03-29 17:27:08 -0500

    Updated migrations and added a test

--- study/tests.py
+++ study/tests.py
@@ -3,3 +3,11 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from .models import StudentRatings, Student
 
+
+class TutorRequestTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create_user(
+            username="harrisonc", password="clark6789")
+
+    def test_number_of_users(self):
+        self.assertEqual(User.objects.all().count(), 1)
