e14539e1dcef582f9fbfeff0ad2e10569407368e
hpathipati <hp4hz@virginia.edu>
2020-04-08 01:28:20 -0400

    added tests

--- userprofile/tests.py
+++ userprofile/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from .forms import EditProfile
 # Create your tests here.
+class testEditProfileForm(TestCase):
+
+    def editProfile_valid_form(self):
+        form = EditProfile.objects.create(first_name = "Harshita", last_name = "Pathipati",student_year = "second")
+        self.assertTrue(form.is_valid())
+
+    def editProfile_valid_form2(self):
+        form2 = EditProfile.objects.create(last_name = "Pathipati",student_year = "second")
+        self.assertFalse(form2.is_valid())
+
