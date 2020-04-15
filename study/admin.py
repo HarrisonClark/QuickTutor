386e941046403a07fdd9b194145bd1fb91477acf
Harrison Clark <harrison.clark99@icloud.com>
2020-04-15 02:31:22 -0500

    Finished rough request system

--- study/admin.py
+++ study/admin.py
@@ -2,6 +2,5 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Student)
-admin.site.register(Tutor)
 admin.site.register(tutorRequest)
 admin.site.register(Course)
--- userprofile/forms.py
+++ userprofile/forms.py
@@ -1,11 +1,13 @@
 
 from django import forms
 from study.models import Student
-from study.models import Tutor
+
 
 class EditProfile(forms.Form):
     first_name = forms.CharField(max_length=25)
     last_name = forms.CharField(max_length=25)
     student_year = forms.CharField(max_length=50, required=False)
-    picture = forms.URLField(required=False, help_text = "Paste URL of desired picture")
-    bio = forms.CharField(max_length=200, widget=forms.Textarea(attrs={"rows":5, "cols":20}), required=False)
+    picture = forms.URLField(
+        required=False, help_text="Paste URL of desired picture")
+    bio = forms.CharField(max_length=200, widget=forms.Textarea(
+        attrs={"rows": 5, "cols": 20}), required=False)
