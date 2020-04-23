a4b6c39b2a5c5882c779a8fb186f32a4c5926ab5
hpathipati <hp4hz@virginia.edu>
2020-04-23 03:21:24 -0400

    added dropdown menu for year

--- userprofile/forms.py
+++ userprofile/forms.py
@@ -6,7 +6,8 @@ from study.models import Student
 class EditProfile(forms.Form):
     first_name = forms.CharField(max_length=25)
     last_name = forms.CharField(max_length=25)
-    student_year = forms.CharField(max_length=50, required=False)
+    # student_year = forms.CharField(max_length=50, required=False)
+    student_year = forms.ChoiceField(choices=[(x, x) for x in range(1, 5)])
     picture = forms.URLField(
         required=False, help_text="Paste URL of desired picture")
     bio = forms.CharField(max_length=200, widget=forms.Textarea(
