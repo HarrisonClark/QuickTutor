d60a1fe3910aa19581f09d5c77e90dba5b2a309b
hpathipati <hp4hz@virginia.edu>
2020-04-24 21:12:13 -0400

    false profile form test

--- userprofile/tests.py
+++ userprofile/tests.py
@@ -11,12 +11,12 @@ class testEditProfileForm(TestCase):
 
     def test_editProfile_valid_form(self):
         form = EditProfile(data={
-            'first_name': "Harshita", 'last_Name': "Pathipati", 'student_year': '2022', 'bio': "", 'picture': ""
+            'first_name': "Harshita", 'last_Name': "Pathipati", "graduation_year": 2022
         })
         self.assertFalse(form.is_valid())
 
     def test_editProfile_valid_form2(self):
         form2 = EditProfile(data={
-            'first_name': "Harshita", 'last_Name': "", 'student_year': 2022, 'bio': "", 'picture': ""
+            'first_name': "Student", 'last_Name': "", 'student_year': 2022, 'bio': "", 'picture': ""
         })
         self.assertFalse(form2.is_valid())
