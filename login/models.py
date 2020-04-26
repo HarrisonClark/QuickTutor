03d129fc1cf70522bde55e0dd962220d5036edc2
Harrison Clark <harrison.clark99@icloud.com>
2020-04-26 18:37:28 -0500

    Removed code for profile pic from social

--- login/models.py
+++ login/models.py
@@ -1,14 +1,1 @@
 from django.db import models
-from allauth.account.signals import user_signed_up
-from django.dispatch import receiver
-from django.contrib.auth.models import User
-from study.models import Student
-
-
-@receiver(user_signed_up)
-def saveProfilePic(request, user, **kwargs):
-    try:
-        user.student.picture = user.socialaccount.extra_data['picture']
-        user.save()
-    except:
-        print("Picture couldn't be saved")
